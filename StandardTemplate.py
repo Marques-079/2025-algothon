56a90663b33f321c0cc1fffd575217b2755f99b5
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-21 03:08:42 +1000

    comments

--- StandardTemplate.py
+++ StandardTemplate.py
@@ -2,12 +2,15 @@ import inspect
 import types
 
 def export(func):
+    """ Add this as a decorator to a function or method which will be exported as getMyPosition"""
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
     wrapper._is_export = True  # Add custom attribute
     return wrapper
 
 def export_trading_funcs():
+    """ Call to export a tradding function with the @export decorator as getMyPosition for evaluation
+    Note, the first function with the decorator will be exported."""
     frame = inspect.currentframe()
     caller_globals = frame.f_back.f_globals
     for name,f in caller_globals.items():
@@ -27,11 +30,15 @@ def find_exports(scope):
                 marked_methods.append(name)
     return marked_methods
 
+
 class Trader():
+    """ Base class for building more complex traders that would hold tate
+    "Using global too many variables is a bit stupid at that point guys " """
     def __init__(self):
         pass
 
     def export_trader(self):
+        """ Exports trader method with @export decorator as getMyPosition for evaluation """
         frame = inspect.currentframe()
         cname = self.__class__
         caller_globals = frame.f_back.f_globals
