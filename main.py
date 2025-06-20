977e5da7c2a56e5836a89944cc862c6bf42cc4dd
Aleksander Pačnik <aleksander.pacnik@gmail.com>
2025-06-20 23:21:24 +1000

    basic scaffold done with indicator + in main

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from requests import ReadTimeout
 from scipy.special import softmax
 import numpy as np
 
+from indicators import rsi_exponential, atr_close_to_close
+
 ##### TODO #########################################
 ### IMPLEMENT 'getMyPosition' FUNCTION #############
 ### TO RUN, RUN 'eval.py' ##########################
@@ -67,18 +69,38 @@ def random(prcSoFar: np.ndarray):
 # Template Indicator Starting
 def MachineOrchestra(prcSoFar):
     global currentPos
+    bought = False
+
+    price_buy = None
+    day_bought = None
+    tp = None
+    sl = None
+    tp_ratio = 10
+    sl_ratio = 5
+
+    currentPos = np.zeros(nins)
 
     # nins is number of instruments
     # nt is number of days in
     (nins, nt) = prcSoFar.shape
 
     # Get all indicators
+    rsi = rsi_exponential(prcSoFar[0, :])
+    atr = atr_close_to_close(prcSoFar[0, :])
 
     # Plug into ML models
+    ## basic for now
+    if rsi[-1] < 30 and not bought:
+        bought = True
+        price_buy = prcSoFar[0, -1]
+        day_bought = nt
+        tp = price_buy + tp_ratio*atr[-1]
+        sl = price_buy - sl_ratio*atr[-1]
+        currentPos[0] = 50
+    
 
     # Adjust from current pos
 
-    currentPos = np.zeros(nins)
     return currentPos
 
 #CHATGPT CODE
