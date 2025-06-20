344b691c5dee61a1afe2680c25fe088a54f5adee
Aleksander Pačnik <aleksander.pacnik@gmail.com>
2025-06-20 15:44:07 +1000

    Indicator notes

---
+++ indicator.py
@@ -0,0 +1,13 @@
+# Will take in price as a vector, and output indicator values as vectors
+# Need separate code to intake prices and pass into the indicators
+
+
+# RSI
+
+# MAC D
+
+# bollinger bands
+
+# other MA
+
+# Volitility
