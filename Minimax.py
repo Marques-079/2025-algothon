da954ef9fee8b2847fdc543d52460dac54c11aa4
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-25 11:16:58 +1000

    Basic setup

--- Minimax.py
+++ Minimax.py
@@ -6,7 +6,14 @@ import numpy as np
 class mmx(Trader):
     def __init__(self):
         super(Trader).__init__()
+        self.first = True
     
     @export
     def minimax(self,prcSoFar: np.ndarray):
-        pass
+        nInst, t = prcSoFar.shape
+        lp = prcSoFar[:,-1]
+        lpmax = 10_000/lp
+
+        if self.first:
+            self.first = False
+            return lpmax
