495ee47c96fe95db871a6e99d8c8aa3cd4d6bc51
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-27 12:47:21 +1000

    Recreated delta split results

--- superiorBaseline.py
+++ superiorBaseline.py
@@ -5,22 +5,47 @@ class superiorBaseline(Trader):
     def __init__(self):
         super(Trader).__init__()
         self.first = True
+        
+        self.nInst = 50
+        self.PLTable = np.zeros((self.nInst,1000))
     
     @export
     def position(self,prcSoFar: np.ndarray):
         nInst,t = prcSoFar.shape
+
         lPrice = prcSoFar[:,-1]
         posMax = 10_000/lPrice
 
         if self.first:
             self.first = False
             return posMax
-
+        
+        pPrice = prcSoFar[:,-2]
+        self.PLTable[:,t] = self.getProfitLoss(lPrice,pPrice) + self.PLTable[:,t-1]
+        self.rankedCumulativeWinLoss()
+        
         marketTrend = self.getMarketTrend(prcSoFar)
-        return posMax*marketTrend
+        position = np.full(50,marketTrend)
+
+        return posMax*position
 
     def sigmoid(self,x):
         return 1 / (1 + np.exp(-x))
+    
+    def getProfitLoss(self,lPrice:np.ndarray,pPrice:np.ndarray):
+        diff = lPrice-pPrice
+        return diff / pPrice * 100
+
+    def rankedCumulativeWinLoss(self):
+        K = 10
+        gdList = []
+        for i in range(self.nInst):
+            wr = np.where( self.PLTable[i,:] > 0, 1, 0 ).sum()
+            gdList.append((i,wr))
+        gdList.sort(key=lambda x:x[1],reverse=True)
+        Ws = [i[0] for i in gdList[:K] ]
+        Ls = [ i[0] for i in gdList[-K:] ]
+        print(Ws,Ls)
 
     def getMarketTrend(self,prcSoFar: np.ndarray):
         nInst,t = prcSoFar.shape
