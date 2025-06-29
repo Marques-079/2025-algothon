ad61128516df01f9d12c6be369941c8fbfc74926
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-29 11:30:34 +1000

    something happend here

--- data/pdf.py
+++ data/pdf.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import gaussian_kde
 
 fn="../prices.txt"
 df=pd.read_csv(fn, sep='\s+', header=None, index_col=None)
@@ -20,21 +19,19 @@ def softmax(x):
 
     
 max_freq_pred = []
-
-density_mat = []
-pmax = instrPrice.max()
-pmin = instrPrice.min()
-padding = (pmax - pmin) * 0.1
-price_grid = np.linspace(pmin - padding, pmax + padding, 500)
+min_freq_pred = []
+mean_freq_pred = []
 
 lookback = 0
+
+
 for i in range(1,len(instrPrice)):
     x_current = instrPrice[i]
+    lookback = max( i-4, 0)
     x_prev = instrPrice[lookback:i]
     diff = x_current-x_prev
     tdiff = list(reversed(range(1,len(x_prev)+1)))
     gradient = diff/tdiff
-    p = gradient.mean()
 
     
     x_next = gradient + x_current 
@@ -47,28 +44,26 @@ for i in range(1,len(instrPrice)):
     max_val = scaled.max()
     full_range = np.arange(min_val, max_val + 1)/100
     counts = np.bincount(scaled)[min_val:]
+
     idx = np.argsort(counts)
-    max_freq_pred.append(np.round(full_range[idx[-1]],decimals=1))
-    prob = softmax(counts)
+    max_val = full_range[idx[-1]]
+    max_val = np.round(max_val,decimals=1) 
+    
+    min_val = full_range[idx[0]]
+    min_val = np.round(min_val,decimals=1) 
 
-    if i == 1:
-        continue
-    # Gaussian KDE
-    # kde = gaussian_kde(instrPrice[max(i-10,0):i+1])
-    kde = gaussian_kde(x_next)
-    density = kde(price_grid)
-    y = density/np.sum(density)
-    density_mat.append(y)
+    mean_val = x_next.mean()
 
-density_mat = np.array(density_mat).T
+    max_freq_pred.append(max_val)
+    min_freq_pred.append(min_val)
+    mean_freq_pred.append(mean_val)
+    
+
+
 plt.figure(figsize=(20,10))
-plt.plot(instrPrice,alpha=0.4)
-plt.plot(range(1,750),max_freq_pred,alpha=0.4)
-plt.imshow(
-    density_mat,
-    aspect='auto',
-    origin='lower',
-    extent=[1, 751, instrPrice.min(), instrPrice.max()],
-    cmap='viridis'
-)
+plt.plot(instrPrice,label="true",marker='o')
+plt.plot(range(2,len(instrPrice)+1),max_freq_pred,label="forecast max",marker='o')
+plt.plot(range(2,len(instrPrice)+1),min_freq_pred,label="forecast min",marker='o')
+plt.plot(range(2,len(instrPrice)+1),mean_freq_pred,label="forecast mean",marker='o')
+plt.legend()
 plt.show()
