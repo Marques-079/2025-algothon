127925408eda48b86c062627ccdff714d591e83f
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-07-01 00:56:34 +1000

    Mean reversion dataloading

--- Model/Dev/MeanReversion/__init__.py
+++ Model/Dev/MeanReversion/__init__.py
@@ -1,2 +1,13 @@
 from .MeanReversionAlg import MeanReversionTrader
-trader = MeanReversionTrader()
+import pickle
+with open("Research/market_condition/mean_reversion_params.pkl", "rb") as f:
+    data = pickle.load(f)
+
+beta_matrix = data["beta_matrix"]
+alpha_matrix = data["alpha_matrix"]
+pairs = data["pairs"]
+if __name__ == "__main__":
+    print(pairs)
+
+# Initialize trader
+trader = MeanReversionTrader(pairs, beta_matrix, alpha_matrix)
