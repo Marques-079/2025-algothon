23a18ff756e9741e34985cd1701ea8afc49e09e8
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-07-13 10:12:39 +1200

    temp

--- Model/Dev/LiX/Trader.py
+++ Model/Dev/LiX/Trader.py
@@ -1,4 +1,11 @@
 from Model.standard_template import Trader,export
+import numpy as np
 
 class LiX(Trader):
-    pass
+    def __init__(self):
+        super(Trader).__init__()
+    
+    @export
+    def make_pos(self,prcSoFar: np.ndarray):
+        return 10_000/prcSoFar[:,-1]
+
