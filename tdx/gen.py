434d4ddaa11d9b37da569f0871a2795491a9ccc8
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-07-12 23:06:40 +1200

    Internally correlated generator

--- tdx/gen.py
+++ tdx/gen.py
@@ -26,13 +26,31 @@ def gen_signals(granularity, instruments=50, T=1000,std=1,base=False,m=1):
 
 
 
-def R1Generator():
-    I = 50
+def R1Generator(I=50):
     # Generate signals of varying granularity
     signals = [
         gen_signals(15, instruments=I, base=True, std=0.9)[1],
-        gen_signals(1000, instruments=I, std=0.45, m=0.9)[1],
+        gen_signals(500, instruments=I, std=0.7, m=0.3)[1],
+        gen_signals(1000, instruments=I, std=0.7, m=0.9)[1],
     ]
+    minvals = np.min(signals,axis=0) 
+    minvals = np.where(minvals < 0,minvals,0)
+    signals -= minvals
     
     combined = np.sum(signals,axis=0)
+    return combined.T
+
+def R1Icorr(RealI=25,rev = True):
+    Reals = R1Generator(RealI).T
+    Generated = []
+    for i in range(50-RealI):
+        sample_idx = np.random.choice(np.arange(RealI),3,replace=False)
+        samples = Reals[:,sample_idx]
+        m = np.mean(samples,axis=1)
+        if rev and np.random.choice([0,1],1) == 0:
+            m = m[::-1]
+        Generated.append(m)
+    
+    stacked = np.stack(Generated).T
+    combined = np.concatenate(( stacked,Reals ),axis=1)
     return combined.T
