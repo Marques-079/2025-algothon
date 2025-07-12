f48e19ecb5ed1bf0f26a8c0e0de387955f9a727b
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-07-13 00:50:42 +1200

    moved things around and made it acutally test selected model

--- tdx/__main__.py
+++ tdx/__main__.py
@@ -1,27 +1,27 @@
-from gen import *
 import matplotlib.pyplot as plt
-
+import sys
+import os
+from gen import *
+from eval import Evaluator
+# Add the parent directory of main_dir/ to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Model.Dev import Insider
+from Model.Dev import *
 import pandas as pd
+
 def loadPrices(fn):
     df = pd.read_csv(fn, sep='\s+', header=None, index_col=None)
     return df
 
 RealData = ( loadPrices("prices.txt").values ).T
+TD = RealData
 
-from eval import Evaluator
-import sys
+T = ActiveModel.trader
+T.export_trader()
 
-import os
-# Add the parent directory of main_dir/ to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from Model.Dev import Insider
-
-
-TD = R1Icorr()
 ref=  Insider.InsideTrader(TD)
 
-e = Evaluator(TD,ref.get_exported())
+e = Evaluator(TD,getMyPosition,ref.get_exported())
 
 t = TD.shape[1]
 e.evaluate(0,t)
