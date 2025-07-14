aa393d01f933cd5aaad5b6ecd2a0530228882782
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-07-14 20:42:23 +1200

    implemented LiX

--- Model/Dev/LiX/Trader.py
+++ Model/Dev/LiX/Trader.py
@@ -13,45 +13,35 @@ class LiX(Trader):
     @export
     def make_pos(self,prcSoFar: np.ndarray):
         _, t = prcSoFar.shape
-        self.lpmax = 10_000/prcSoFar[:,-1]
-        N = 50
+        current_price = prcSoFar[:,-1]
+        self.lpmax = 10_000/current_price
+        N =  1000
         if self.first:
             self.first = False
             for i in range(0,N):
-                self.emaTable.append(self.pregen_ema(prcSoFar,span=2*( i+1 )))
+                self.emaTable.append(self.pregen_ema(prcSoFar,span=i))
 
         curr_emas = []
         for i in range(0,N):
             emas = self.emaTable[i]
-            new_ema = self.get_next_ema(prcSoFar,span=2*(i+1))
+            new_ema = self.get_next_ema(prcSoFar,span=i)
             curr_emas.append(new_ema)
             new_ema = new_ema[:,np.newaxis]
             self.emaTable[i] = np.concat(( emas,new_ema ),axis=1)
         curr_emas = np.array(curr_emas)
         cm_mean = np.mean(curr_emas,axis=0)
         cm_std = np.std(curr_emas,axis=0)
-        print(curr_emas.shape,cm_mean.shape,cm_std.shape,cm_std[15],t)
+        
+        trade_signal = current_price - cm_mean
+        trade_signal /= np.abs(trade_signal)
+        
+        weight = np.where(cm_std>1,1,cm_std)
 
-        if t == 999:
-            pemas = np.stack(self.emaTable)
-            print(pemas.shape)
-            ipPema = pemas[:,15,:].T
-            emas = ipPema
-            convergence_measure = np.mean(emas,axis=1)
-            stdT = np.std(emas,axis=1) 
-            stdB = -stdT
-            std = np.stack([stdT+convergence_measure,stdB+convergence_measure])
-            print(emas[-10:],stdT[-10:],convergence_measure[-10:])
-            plt.figure(figsize=(10, 5))
-            plt.plot(prcSoFar[15,:], label='Noisy Measurements', alpha=0.5)
-            plt.plot(emas,alpha=0.1,color="red")
-            plt.plot(convergence_measure,label="convergence measure")
-            plt.plot(std.T,color="yellow")
-            plt.show()
-
-        return self.lpmax 
+        pos = trade_signal * self.lpmax
+        return pos
 
     def pregen_ema(self,mat,span):
+        span = self.span_transform(span)
         alpha = 2 / (span + 1)
         nInst,T = mat.shape
         T -= 1
@@ -62,8 +52,12 @@ class LiX(Trader):
         return ema
     
     def get_next_ema(self,mat,span):
+        spandex = span
+        span = self.span_transform(span)
         alpha = 2 / (span + 1)
-        spandex = int(span/2 - 1)
         ema = self.emaTable[spandex]
         new_ema = alpha * mat[:,-1] + (1 - alpha) * ema[:,-1]
         return new_ema
+
+    def span_transform(self,s):
+        return 0.5*(s+1)
