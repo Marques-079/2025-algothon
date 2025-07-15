655ba4770521755a56466e503b6f816433dbd925
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-07-15 19:45:00 +1200

    just something else that doesnt work

--- Model/Dev/CalciumV/Trader.py
+++ Model/Dev/CalciumV/Trader.py
@@ -6,13 +6,11 @@ class CalciumV(Trader):
         super(Trader).__init__()
         self.lpmax = np.zeros(50)
         self.first = True
-        self.first_ema = True
-        self.window_length = 15
+        self.window_length = 20
         self.score_table = np.array([[]]*50)
         self.mean_start = np.zeros(50)
+        self.scaled_z_thresh = 0.3
     
-        self.emaRange = 10
-        self.emaTable = []
     @export
     def make_pos(self,prcSoFar: np.ndarray):
         _, t = prcSoFar.shape
@@ -22,19 +20,30 @@ class CalciumV(Trader):
             self.first = False
             self.hot_start_prep(prcSoFar)
             
-        price_window = prcSoFar[:,t-self.window_length-1:t]
-        scaled_z, z,_score = self.do_one_update(price_window)
-        self.mean_start = np.where(scaled_z > 0.3, t, self.mean_start)
-        
+        price_window = prcSoFar[:,t-self.window_length-10:t]
+        scaled_z, z,score = self.do_one_update(price_window)
+        self.mean_start = np.where(scaled_z > self.scaled_z_thresh, t, self.mean_start)
 
-        return np.zeros(50)
+        means = np.zeros(50)
+        for i in range(50):
+            start = int(self.mean_start[i])
+            relevant_range = prcSoFar[i,start-2:]
+            means[i] = np.mean(relevant_range)
+        
+        direction = current_price - means
+        direction = np.where(direction != 0, direction/np.abs(direction),0)
+
+        activation = np.where(scaled_z > self.scaled_z_thresh, 1, 0)
+
+            
+        return direction * activation * lpmax
 
     def hot_start_prep(self,prcSoFar: np.ndarray):
         _, t = prcSoFar.shape
         for i in range(self.window_length+1,t):
             price_window = prcSoFar[:,i-self.window_length-1:i]
             scaled_z,_,_ = self.do_one_update(price_window)
-            self.mean_start = np.where(scaled_z > 0.3, i, self.mean_start)
+            self.mean_start = np.where(scaled_z > self.scaled_z_thresh, i, self.mean_start)
     
     def do_one_update(self,price_window: np.ndarray):
         diff_window = np.diff(price_window,axis=1)
